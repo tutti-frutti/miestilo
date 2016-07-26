44eee9ddf1ace64c170e76df8ca7834740bca84c
Anton <anton.verstka@gmail.com>
2016-07-26 18:31:14 +0300

    fancybox

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ $('.main-nav__toogle').click(function(){
 //    $('.main-nav__items-left').slideToggle();
 //    $('.main-nav__items-right').slideToggle();
 });
+
+$(document).ready(function() {
+		$(".fancybox").fancybox();
+	});
+
+$(document).ready(function() {
+	$(".fancybox").fancybox({
+		openEffect	: 'fade',
+		closeEffect	: 'none'
+	});
+});
